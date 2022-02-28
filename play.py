7eab2e882f68e9496c61132314e4857fde057704
Victoooooor <victorzh716@gmail.com>
2022-02-28 18:48:41 -0500

    add

--- play.py
+++ play.py
@@ -7,20 +7,18 @@ import numpy as np
 #ffpyplayer for playing audio
 from ffpyplayer.player import MediaPlayer
 
+n_channels = 5
 
 y, sr = librosa.load('./out.mp4')
-chroma = librosa.feature.chroma_cqt(y = y, sr = sr,) #n_chroma = 5
+chroma = librosa.feature.chroma_cqt(y = y, sr = sr,n_chroma = n_channels, bins_per_octave = n_channels * 3) #n_chroma = 5
+
 dura = librosa.get_duration(y = y, sr = sr)
-print(dura)
-print(chroma.shape)
+
 fps = 28
-length = round(dura * fps)
+length = dura * fps
+chroma_len = chroma.shape[1] - 1
+window_map = chroma_len / length
 
-window_map = (chroma.shape[1] - 1)/(length-1)
-
-print(chroma[0])
-# print(y.shape)
-# print(sr)
 board = pyfirmata.Arduino('/dev/ttyACM0')
 m1 = board.get_pin("d:13:s")
 m2 = board.get_pin("d:12:s")
@@ -30,6 +28,9 @@ m5 = board.get_pin("d:9:s")
 
 video_path="./out.mp4"
 video = cv2.VideoCapture(video_path)
+fps = video.get(cv2.CAP_PROP_FPS)
+wait = round(1000/fps)
+print(fps)
 player = MediaPlayer(video_path)
 index = 0
 while True:
@@ -38,13 +39,20 @@ while True:
     if not grabbed:
         print("End of video")
         break
-    if cv2.waitKey(fps) & 0xFF == ord("q"):
+    if cv2.waitKey(wait) & 0xFF == ord("q"):
         break
     cv2.imshow("Video", frame)
     if val != 'eof' and audio_frame is not None:
         # audio
         img, t = audio_frame
-    m1.write(180*chroma[0][round(index * window_map)])
+
+    pos = round(index * window_map)
+    pos %= chroma_len
+    m1.write(180*chroma[0][pos])
+    m2.write(180 * chroma[1][pos])
+    m3.write(180 * chroma[2][pos])
+    m4.write(180 * chroma[3][pos])
+    m5.write(180 * chroma[4][pos])
     index += 1
 video.release()
 cv2.destroyAllWindows()
